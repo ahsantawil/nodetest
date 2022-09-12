9d73dd6052f8dc19cc9c941a8b44174aad4cbb0b
ahsantawil <ahsan.tawil07@gmail.com>
2022-09-12 10:27:13 +0700

    npm dependency

---
+++ npm/hello.js
@@ -0,0 +1,11 @@
+import {sayHello, sum} from 'belajar-library-nodejs';
+import {min, max} from 'belajar-library-nodejs/numbers';
+
+console.log(sayHello("Ahsan"));
+
+const result = [10, 10, 10, 10, 10];
+console.log(sum(result));
+
+
+console.log(min(10, 20));
+console.log(max(10, 20));
